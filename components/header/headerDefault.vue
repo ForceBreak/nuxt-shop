bb56ff7b1fc39edca47fe98321e67c974766cf37
dpr_fan <gbuecjd18@gmail.com>
2020-04-24 22:30:20 +0300

    update lang component

--- components/header/headerDefault.vue
+++ components/header/headerDefault.vue
@@ -13,8 +13,32 @@
       <v-spacer></v-spacer>
 
       <client-only>
-        <VuetifyI18nLanguageSwitcherNuxt :languages="mixin_locales"/>
-
+        <v-menu offset-y>
+          <template v-slot:activator="{ on }">
+            <v-btn
+              dark
+              icon
+              v-on="on"
+            >
+              <v-icon>mdi-earth</v-icon>
+            </v-btn>
+          </template>
+          <v-list>
+            <v-list-item
+              v-for="(item, index) in mixin_locales"
+              :key="index"
+              nuxt
+              :to="switchLocalePath(item.id)"
+            >
+              <v-list-item-avatar width="25" min-width="25" height="25" min-height="25" horizontal>
+                <v-img :src="item.flagSrc"></v-img>
+              </v-list-item-avatar>
+              <v-list-item-title>
+                {{ item.title }}
+              </v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
         <v-btn
           v-if="isLoggedIn"
           icon
@@ -47,14 +71,12 @@
 
 <script>
   import { mapGetters } from 'vuex'
-  import VuetifyI18nLanguageSwitcherNuxt from 'vuetify-i18n-language-switcher-nuxt'
 
   import downloadLoader from '~/components/loaders/downloadLoader'
 
   export default {
     components: {
       downloadLoader,
-      VuetifyI18nLanguageSwitcherNuxt
     },
     computed: {
       ...mapGetters({
